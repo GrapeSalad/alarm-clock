6e2a7b7522bba51c0620fe41b35290824470059c
Anabel Ramirez and David Wilson <anabel+david@>
2017-06-27 11:52:43 -0700

    add alarm field, working on time difference.

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
-  var now = moment().format("dddd, hh:mmA");
+  var now = moment().format("HH:mmA");
   $('#time').append('<p>The time is ' + now + ' now is the time.</p>');
   $("#alarm-set").submit(function(event){
     event.preventDefault();
     var alarm = $("#alarm").val();
     console.log("alarm is set for:" + alarm);
+    var alarmCountdownTime = moment().subtract(moment(alarm.toString(), "HH:mmA"),moment(now.toString(), "HH:mmA"));
+    console.log(moment(alarm.toString(), "HH:mmA"));
+    console.log("time till alarm: " + moment(alarmCountdownTime).format("HH:mmA"));
   });
 });
